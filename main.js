7b6dd8248bb64ae611fb1823cf33055c9aba8ebc
Anitah Aturinda <104755087+anitahx@users.noreply.github.com>
2022-09-06 15:55:20 +0300

    Update main.js
    final javascript

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const btn = document.getElementById("Calculate")
     btn.addEventListener("click",(event) => {
     event.preventDefault()
-    heightMeters = document.getElementById('height_meters').value; 
+
+    heightMeters = document.getElementById('height_feet').value; 
     heightInches = document.getElementById('height_inches').value; 
-    weight = document.getElementById("weight").value;
+    weight = document.getElementById('weight_kg').value;
     
     document.getElementById('bmi_metric_result').textContent = (weight/ (heightMeters * heightMeters))
-    document.getElementById('bmi_imperial_result').textContent = (783 * (weight/ heightInches * heightInches))
+    document.getElementById('bmi_imperial_result').textContent = (783 * (weight/ (heightInches * heightInches)))
 
     console.log(bmi_metric_result)
     console.log(bmi_imperial_result)
 })
+
+
